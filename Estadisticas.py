29cb89b52c447df3d485a4d8e3626a4b93ed2e27
Manuela Machado <manuela.machado@correo.unimet.edu.ve>
2026-08-08 04:24:58 +0000

    actualizado

--- Estadisticas.py
+++ Estadisticas.py
@@ -5,3 +5,9 @@ En este archivo gestionamos los análisis y métricas del sistema:
 3. Calcular el promedio general de temperaturas de las consultas activas.
 4. Consultar datos históricos en Open-Meteo y graficar con Pandas y Matplotlib.
 """
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+from Modelos import Municipio, RegistroClima
+
