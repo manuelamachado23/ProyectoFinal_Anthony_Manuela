ab1435db9d1f65199b8f1ee2b296fe444ff29ec1
anthonyfd1a-ui <anthonyfd1a@gmail.com>
2026-08-08 04:02:35 +0000

    actualizado

--- Servicios.py
+++ Servicios.py
@@ -46,3 +46,63 @@ class ServicioApiClima:
    # Clase encargada de realizar las peticiones a la API de Open-Meteo.
 
    URL_BASE: str = "https://api.open-meteo.com/v1/forecast"
+
+   @classmethod
+   def mapear_codigo_wmo(cls, codigo: int) -> str:
+       # Traduce el número de código meteorológico WMO.
+       codigos_wmo = {
+           0: "Despejado",
+           1: "Principalmente despejado",
+           2: "Parcialmente nublado",
+           3: "Nublado",
+           45: "Niebla",
+           48: "Niebla con escarcha",
+           51: "Llovizna ligera",
+           53: "Llovizna moderada",
+           55: "Llovizna densa",
+           61: "Lluvia ligera",
+           63: "Lluvia moderada",
+           65: "Lluvia fuerte",
+           80: "Chubascos ligeros",
+           81: "Chubascos moderados",
+           82: "Chubascos violentos",
+           95: "Tormenta eléctrica",
+       }
+       return codigos_wmo.get(codigo, "Estado desconocido / No registrado")
+
+   @classmethod
+   def consultar_clima_actual(
+       cls, municipio: str, localidad: str, latitud: float, longitud: float
+   ) -> RegistroClima | None:
+       # Realiza la solicitud web a Open-Meteo y retorna un objeto RegistroClima.
+       parametros = {
+           "latitude": latitud,
+           "longitude": longitud,
+           "current": ["temperature_2m", "relative_humidity_2m", "wind_speed_10m", "weather_code"]
+       }
+
+       try:
+           respuesta = requests.get(cls.URL_BASE, params=parametros, timeout=10)
+           respuesta.raise_for_status()
+           datos_json = respuesta.json()
+           datos_current = datos_json.get("current", {})
+           temp = datos_current.get("temperature_2m", 0.0)
+           hum = int(datos_current.get("relative_humidity_2m", 0))
+           vie = datos_current.get("wind_speed_10m", 0.0)
+           wmo = datos_current.get("weather_code", 0)
+           estado_texto = cls.mapear_codigo_wmo(wmo)
+
+           return RegistroClima(
+               municipio=municipio,
+               localidad=localidad,
+               latitud=latitud,
+               longitud=longitud,
+               temperatura=temp,
+               humedad=hum,
+               viento=vie,
+               estado_tiempo=estado_texto
+           )
+
+       except requests.exceptions.RequestException as error:
+           print(f" Error de conexión con la API: {error}")
+           return None
